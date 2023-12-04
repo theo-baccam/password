40bb1acc44213fe995e1c47746370cba11ca5eb8
theo-baccam <theo.baccam@laplateforme.io>
2023-12-04 14:30:41 +0100

    refactoring + commentaire

--- password.py
+++ password.py
@@ -1,26 +1,32 @@
+# Permet d'importer string.punctuation, car il n'y a pas de string method pour les caractères spéciaux.
 import string
 
+# Pour avoir un input qui ne montre pas les caractères qui sont tapés.
 import getpass
 
+# Module pour les algorithmes de hashing.
 import hashlib
-sha256 = hashlib.sha256()
 
 
-def is_valid(input_string):
-    punctuations = [punctuation for punctuation in string.punctuation]
-
-    if (
-        len(input_string) >= 8
-        and any(character.islower() for character in input_string)
-        and any(character.isupper() for character in input_string)
-        and any(character.isdigit() for character in input_string)
-        and any(character in punctuations for character in input_string)
-    ):
-        return True
-    else:
-        return False
+# Permet de vérifier si le mot de passe qui est entré correspond aux critères.
+def is_valid(password):
+    return (
+        len(password) >= 8
+        and any(character.islower() for character in password)
+        and any(character.isupper() for character in password)
+        and any(character.isdigit() for character in password)
+        and any(character in string.punctuation for character in password)
+    )
 
 
+# Pour encoder en SHA-256.
+def hash_password(password):
+    sha256 = hashlib.sha256()
+    sha256.update(password.encode("ascii"))
+    return sha256.hexdigest()
+
+
+# Boucle principale
 while True:
     password = getpass.getpass("Entrez un mot de passe: ")
 
@@ -29,9 +35,7 @@ while True:
         continue
     else:
         print("Le mot de passe est valide!")
+        password_hash = hash_password(password)
+        print(f"Le hash du mot de passe:\n{password_hash}")
         break
 
-sha256.update(password.encode("ascii"))
-
-password_hash = sha256.hexdigest()
-print(f"Le hash du mot de passe:\n{password_hash}")
