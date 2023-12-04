e89bdca172998e5d1c3bafd1b2d8c2e39db12dbd
theo-baccam <theo.baccam@laplateforme.io>
2023-12-04 14:04:05 +0100

    restructuration code et ajout fonction hash

--- password.py
+++ password.py
@@ -2,30 +2,36 @@ import string
 
 import getpass
 
-punctuations = [punctuation for punctuation in string.punctuation]
+import hashlib
+sha256 = hashlib.sha256()
+
+
+def is_valid(input_string):
+    punctuations = [punctuation for punctuation in string.punctuation]
+
+    if (
+        len(input_string) >= 8
+        and any(character.islower() for character in input_string)
+        and any(character.isupper() for character in input_string)
+        and any(character.isdigit() for character in input_string)
+        and any(character in punctuations for character in input_string)
+    ):
+        return True
+    else:
+        return False
+
 
 while True:
     password = getpass.getpass("Entrez un mot de passe: ")
 
-    if len(password) >= 8:
-        length_good = True
+    if not is_valid(password):
+        print("Le mot de passe n'est pas valide")
+        continue
     else:
-        length_good = False
-
-    contains_lowercase = any(character.islower() for character in password)
-    contains_uppercase = any(character.isupper() for character in password)
-    contains_digit = any(character.isdigit() for character in password)
-    contains_punctuation = any(character in punctuations for character in password)
-
-
-    if (
-        length_good == True and
-        contains_lowercase == True and
-        contains_uppercase == True and
-        contains_digit == True and
-        contains_punctuation == True
-        ):
-        print("Le mot de passe est valide et sera enregistré")
+        print("Le mot de passe est valide!")
         break
-    else:
-        print("Le mot passe est invalide, veuillez re-essayé")
+
+sha256.update(password.encode("ascii"))
+
+password_hash = sha256.hexdigest()
+print(f"Le hash du mot de passe:\n{password_hash}")
